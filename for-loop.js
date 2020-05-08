78c87eebd1f0d2b18a9f175ce5d0788df365a99d
Chad <chdabney42@gmail.com>
2020-05-08 18:10:09 -0400

    notes

---
+++ for-loop.js
@@ -0,0 +1,20 @@
+// for loop
+
+//  for              ()            {}
+//  ^statement       ^parameter    ^code block
+
+// for (let i = 0;) { }
+//           ^starting point. initilaize variable at 0 in the parameter. Terminate with semi-colon
+
+// for (let i = 0; i <= 10;)
+//                    ^as long as variable is less than or equal to this value
+
+
+// for (let i = 0; i <= 10; i++ || i--)
+//                               ^increment or decrement variable. No semicolon needed at end.
+
+
+
+//             for (let i = 0; i <= 10; i++) {
+//                 output(i)
+//             }
---
+++ while -loop.js
@@ -0,0 +1,16 @@
+// while loop
+
+
+// while (condition) {
+//  while this is true, will run this code block. 
+// will come back and check if true then run again
+//  will keep going until false
+// } 
+
+
+// let counter = 1  <-- defining counter variable as 1
+// while (counter <= 3) {  <-- while counter variable is less than or equal to 3/conditonal that could fail eventually
+//     counter += 1  <-- increment counter by 1 change over time every iteration
+// }  
+
+// when counter = 4 code completed and will loop until false
